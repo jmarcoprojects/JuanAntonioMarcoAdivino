95abb4f3d7fe42f0953057799b09141f5e32894a
J M <J M@LAPTOP-HKV40MR6>
2019-11-09 21:15:45 +0100

    entre 10 y 50

--- src/juanantoniomarcoadivino/JuanAntonioMarcoAdivino.java
+++ src/juanantoniomarcoadivino/JuanAntonioMarcoAdivino.java
@@ -18,11 +18,11 @@ public class JuanAntonioMarcoAdivino {
      */
     public static void main(String[] args) {
         int n = 0,cont = 0;
-        int x = (int) (500 *Math.random());
+        int x = (int) (Math.random()*(50-10+1)+10);
         //System.out.print(x);
         Scanner sc = new Scanner(System.in);
         do {
-            System.out.print("\nIntroduce el valor de X: ");
+            System.out.print("\nIntroduce un valor de X entre 10 y 50: ");
             n = sc.nextInt();
             if (n > x) {
                 System.out.print("Te pasaste Amigo");
@@ -31,7 +31,7 @@ public class JuanAntonioMarcoAdivino {
             }
             cont++;
         } while (n != x);
-        System.out.print("Exelente!!! Haz acertado , No. de intentos: "+cont );
+        System.out.println("Exelente!!! Haz acertado , No. de intentos: "+cont );
         sc.close();
     }
 }
